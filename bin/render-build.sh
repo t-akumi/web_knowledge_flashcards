c1de2e357ef5302dba940f0f099e8110d25f0944
takumi <sakatagintoki78013@gmail.com>
2026-03-16 02:47:01 +0900

    render用ビルドスクリプトを追加

---
+++ bin/render-build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -o errexit
+
+bundle install
+
+# アセットを使っているなら（通常は必要）
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+
+# DBマイグレーション（本番は必須）
+bundle exec rails db:migrate
